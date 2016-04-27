8166f9715a3a00cd47575b98122792066aba3680
vagrant <vagrant@homestead>
2016-04-27 14:03:56 +0000

    initial commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>About</title>
+	</head>
+	<body>
+		<h1>About page</h1>
+		<p>This is an application i am making as part of the fitl course. it cost a fortune but so far so good</p>
+	</body>	
+</html>
---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Contact</title>
+	</head>
+	<body>
+		<h1>Contact page</h1>
+		<p>There will be a form here to allow people to contact me</p>
+	</body>	
+</html>
